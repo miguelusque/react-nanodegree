a014570bbb143952acda8ee34f46466c4bd90c06
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-07-02 05:25:17 +0200

    Add contacts search box

--- 06_contacts/src/ListContacts.js
+++ 06_contacts/src/ListContacts.js
@@ -1,31 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
-function ListContacts (props) {
-	return (
-		<ol className='contact-list'>
-		{
-			props.contacts.map((contact) => (
-				<li key={contact.id} className='contact-list-item'> 
-					<div className='contact-avatar' style={{
-						backgroundImage: `url(${contact.avatarURL})`
-					}}/>
-					<div className='contact-details'>
-						<p>{contact.name}</p>
-						<p>{contact.email}</p>
-					</div>
-					<button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-						Remove
-					</button>
-				</li>
-		))}
-		</ol>
-	)
+class ListContacts extends Component {
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+		onDeleteContact: PropTypes.func.isRequired
+	}
+
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState( {query: query.trim()})
+	}
+
+	render() {
+		let showingContacts
+		if (this.state.query) {
+			const match = new RegExp(escapeRegExp(this.state.query), 'i')
+			showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+
+		} else {
+			showingContacts = this.props.contacts
+		}
+
+		showingContacts.sort(sortBy('name'))
+
+		return (
+			<div className='list-contacts'>
+				<div className='list-contacts-top'>
+					<input className='search-contacts'
+						type='text'
+						placeholder='Search contacts'
+						value={this.state.query}
+						onChange={ (event) => this.updateQuery(event.target.value)}
+					/>
+				</div>
+
+				<ol className='contact-list'>
+					{ showingContacts.map((contact) => (
+						<li key={contact.id} className='contact-list-item'> 
+							<div className='contact-avatar' style={{
+								backgroundImage: `url(${contact.avatarURL})`
+							}}/>
+							<div className='contact-details'>
+								<p>{contact.name}</p>
+								<p>{contact.email}</p>
+							</div>
+							<button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+								Remove
+							</button>
+						</li>
+					))}
+				</ol>
+			</div>
+
+		)	
+	}
 }
 
-ListContacts.propTypes = {
-	contacts: PropTypes.array.isRequired,
-	onDeleteContact: PropTypes.func.isRequired
-}
+
 
 export default ListContacts
