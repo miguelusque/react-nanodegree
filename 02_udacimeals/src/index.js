14b9acef1e2759d6bc6098d8779615d430008436
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-10-29 09:42:43 +0100

    Redux support added

--- 02_udacimeals/src/index.js
+++ 02_udacimeals/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux'
+import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
 const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
