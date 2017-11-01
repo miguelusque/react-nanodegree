a74a211879940433b6d182ede54044481a6194f5
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-01 08:16:01 +0100

    Add missing semicolon

--- 02_udacimeals/src/index.js
+++ 02_udacimeals/src/index.js
@@ -7,15 +7,13 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import { Provider } from 'react-redux'
 
-
-
 const logger = store => next => action => {
   console.group(action.type)
   console.info('dispatching', action)
   let result = next(action)
   console.log('next state', store.getState())
   console.groupEnd(action.type)
-  return result
+  return result;
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
