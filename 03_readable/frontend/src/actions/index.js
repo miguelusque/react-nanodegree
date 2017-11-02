8767cec7350ab6578628b949c167ea4aad56eea5
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-02 22:07:51 +0100

    Add 'filterPostsByCategory' and 'sortPostsBy' actions

--- 03_readable/frontend/src/actions/index.js
+++ 03_readable/frontend/src/actions/index.js
@@ -1,16 +1,12 @@
 export const FILTER_POSTS_BY_CATEGORY = 'FILTER_POSTS_BY_CATEGORY';
 export const SORT_POSTS_BY = 'SORT_POSTS_BY';
 
-export function filterPostsByCategory(category) {
-	return {
-		type: FILTER_POSTS_BY_CATEGORY,
-		category
-	}
-}
+export const filterPostsByCategory = category => ({
+  type: FILTER_POSTS_BY_CATEGORY,
+  category
+});
 
-export function sortPostsBy(field) {
-	return {
-		type: SORT_POSTS_BY,
-		field
-	}
-}
+export const sortPostsBy = field => ({
+  type: SORT_POSTS_BY,
+  field
+});
