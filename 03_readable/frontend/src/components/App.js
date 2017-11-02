d00c1c5de229793ad762089649b54195e82a21a6
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-02 09:14:38 +0100

    Add category and post lists

--- 03_readable/frontend/src/components/App.js
+++ 03_readable/frontend/src/components/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { fetchRecipes } from '../utils/api'
+import { connect } from 'react-redux';
+import * as ReadableAPI from '../utils/api';
+import Posts from './Posts';
+import Categories from './Categories';
+import {SORT_BY, PostsSortByPicker} from './PostsSortByPicker';
+
 
 import './App.css';
 
-class App extends Component {
+class App extends Component {  
+  state = {
+    categories: [],
+    posts: [],
+    sortBy: SORT_BY.voteScore
+  }
+
+  componentDidMount() {
+
+    ReadableAPI.fetchPosts().then((posts) => {
+      this.setState({posts:posts});
+    });
+
+    ReadableAPI.fetchCategories().then((categories) => {
+      this.setState(categories);
+    });
+
+  }
+
   render() {
+    const { categories, posts } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='pageContainer'>
+        <header>
+          <h1 className='pageTitle'>Readable</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Categories categories={categories}/>
+        <Posts posts={posts}/>
       </div>
     );
   }
