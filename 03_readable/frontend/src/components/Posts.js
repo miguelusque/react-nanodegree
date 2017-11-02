808a651055fd07da183e3a1970b9d150c0116e53
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-02 19:03:09 +0100

    Refactor as a class

--- 03_readable/frontend/src/components/Posts.js
+++ 03_readable/frontend/src/components/Posts.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import { timestampToString } from '../utils/helpers'
-import './Posts.css';
+import './css/Posts.css';
 
-export default function Posts ({ posts = [] }) {
-  // Sort posts by voteScore desc
-  posts.sort((a,b) => b.voteScore - a.voteScore);
-  posts = posts.sort();
+class Posts extends Component {
+  static propTypes = {
+    posts: PropTypes.array.isRequired
+  }
 
-  return (
-    <div className='postsContainer'>
-      <h3 className='postsHeader'>Posts</h3>
-        {posts.map((post) => (
-          <div className='postContainer' key={post.id}>
-            <div className='postTitle'>{post.title}</div>
-            <div className='postBody'>{post.body}</div>
-            <div className='postDate'>Published: {timestampToString(post.timestamp)}</div>
-            <div className='postAuthor'>Author: {post.author}</div>
-            <div className='postCategory'>Category: {post.category}</div>
-            <div className='postScore'>Score: {post.voteScore}</div>
-          </div>
-        ))}
-    </div>
-  )
+  render() {
+    let {posts} = this.props;
+
+    if (!posts) {
+      posts = [];
+    }
+
+    // Sort posts by voteScore desc
+    posts.sort((a,b) => b.voteScore - a.voteScore);
+    posts = posts.sort();
+
+    return (
+      <div className='postsContainer'>
+        <h3 className='postsHeader'>Posts</h3>
+          {posts.map((post) => (
+            <div className='postContainer' key={post.id}>
+              <div className='postTitle'>{post.title}</div>
+              <div className='postBody'>{post.body}</div>
+              <div className='postDate'>Published: {timestampToString(post.timestamp)}</div>
+              <div className='postAuthor'>Author: {post.author}</div>
+              <div className='postCategory'>Category: {post.category}</div>
+              <div className='postScore'>Score: {post.voteScore}</div>
+            </div>
+          ))}
+      </div>
+    )
+  }
 }
+
+export default Posts
