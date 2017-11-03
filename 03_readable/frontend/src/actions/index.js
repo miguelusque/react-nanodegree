32d7dcab43524bc01ca46a97aa14c808560c7fce
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-03 06:29:19 +0100

    Add 'posts' actions

--- 03_readable/frontend/src/actions/index.js
+++ 03_readable/frontend/src/actions/index.js
@@ -1,5 +1,13 @@
+export const LOAD_POSTS = 'LOAD_POSTS';
 export const FILTER_POSTS_BY_CATEGORY = 'FILTER_POSTS_BY_CATEGORY';
 export const SORT_POSTS_BY = 'SORT_POSTS_BY';
+
+export const SORT_BY = {voteScore: 'voteScore', timestampp: 'timestampp'};
+
+export const loadPosts = posts => ({
+  type: LOAD_POSTS,
+  posts
+});
 
 export const filterPostsByCategory = category => ({
   type: FILTER_POSTS_BY_CATEGORY,
