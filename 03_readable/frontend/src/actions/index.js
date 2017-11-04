61dab6d62e8b1e1495b0ec717f7e98612df0439f
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-04 20:11:50 +0100

    Reorder actions

--- 03_readable/frontend/src/actions/index.js
+++ 03_readable/frontend/src/actions/index.js
@@ -1,10 +1,15 @@
 export const LOAD_POSTS = 'LOAD_POSTS';
-export const FILTER_POSTS_BY_CATEGORY = 'FILTER_POSTS_BY_CATEGORY';
 export const SORT_POSTS_BY = 'SORT_POSTS_BY';
+export const FILTER_POSTS_BY_CATEGORY = 'FILTER_POSTS_BY_CATEGORY';
 
 export const loadPosts = posts => ({
   type: LOAD_POSTS,
   posts
+});
+
+export const sortPostsBy = field => ({
+  type: SORT_POSTS_BY,
+  field
 });
 
 export const filterPostsByCategory = category => ({
@@ -12,10 +17,7 @@ export const filterPostsByCategory = category => ({
   category
 });
 
-export const sortPostsBy = field => ({
-  type: SORT_POSTS_BY,
-  field
-});
+
 
 
 
