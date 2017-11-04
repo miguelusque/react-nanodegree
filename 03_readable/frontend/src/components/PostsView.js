933c6190093deab5a1180d462157b8acb758c7cf
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-04 22:14:50 +0100

    Refactor using 'Categories' component

--- 03_readable/frontend/src/components/PostsView.js
+++ 03_readable/frontend/src/components/PostsView.js
@@ -2,22 +2,13 @@ import React, { Component } from 'react';
 import Posts from './Posts';
 import Categories from './Categories';
 import SortPostsBy from './SortPostsBy';
-
 import './css/PostsView.css';
 
 class PostsView extends Component {
-
-
   render() {
-    const { categories } = this.props
-
     return (
       <div className='postsViewContainer'>
-        <Categories
-          categories={categories}
-/*          onSelect={(category) => {filterPostsByCategoryRequest(category)}}
-*/
-        />
+        <Categories/>
         <SortPostsBy/>
         <Posts/>
       </div>
