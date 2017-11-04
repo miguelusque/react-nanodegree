4e9bc17f79c25a5c9e74cdf3e1e9c424d7a635aa
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-04 17:37:06 +0100

    Load 'categories'

--- 03_readable/frontend/src/components/Categories.js
+++ 03_readable/frontend/src/components/Categories.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { fetchCategories }  from '../utils/api';
+
 import './css/Categories.css';
 
-export default function Categories ({ categories = [], onSelect }) {
-  return (
-    <div className='categoriesContainer'>
-      <h3 className='categoriesHeader'>Categories</h3>
-        <ul className='categoryNames'>
-        {categories.map((category) => (
-          <li className='categoryName' onClick={() => onSelect(category)} key={category.path}>{category.name}</li>
-        ))}
-        </ul>
-    </div>
-  )
+class Categories extends Component {
+  state = {
+    categories: []
+  };
+
+  componentDidMount() {
+    fetchCategories().then((categories) => {
+      this.setState(categories);
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+
+    return (
+      <div className='categoriesContainer'>
+        <h3 className='categoriesHeader'>Categories</h3>
+          <ul className='categoryNames'>
+          {categories.map((category) => (
+            <li className='categoryName' key={category.path}>{category.name}</li>
+          ))}
+          </ul>
+      </div>
+    )
+  }
 }
+
+export default Categories;
