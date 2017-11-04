84ef120039b9d011943caae56395df857dd0f567
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-04 13:07:34 +0100

    Refactor css

--- 03_readable/frontend/src/components/Categories.js
+++ 03_readable/frontend/src/components/Categories.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import './Categories.css';
+import './css/Categories.css';
 
-export default function Categories ({ categories = [] }) {
+export default function Categories ({ categories = [], onSelect }) {
   return (
     <div className='categoriesContainer'>
       <h3 className='categoriesHeader'>Categories</h3>
         <ul className='categoryNames'>
         {categories.map((category) => (
-            <li className='categoryName' key={category.path}>{category.name}</li>
+          <li className='categoryName' onClick={() => onSelect(category)} key={category.path}>{category.name}</li>
         ))}
         </ul>
     </div>
