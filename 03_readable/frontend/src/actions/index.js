a90dd3f9456eafe4f4a0e783e9528c9757e33e6c
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-04 17:16:08 +0100

    Refactor 'SORT_BY'

--- 03_readable/frontend/src/actions/index.js
+++ 03_readable/frontend/src/actions/index.js
@@ -2,8 +2,6 @@ export const LOAD_POSTS = 'LOAD_POSTS';
 export const FILTER_POSTS_BY_CATEGORY = 'FILTER_POSTS_BY_CATEGORY';
 export const SORT_POSTS_BY = 'SORT_POSTS_BY';
 
-export const SORT_BY = {voteScore: 'voteScore', timestampp: 'timestampp'};
-
 export const loadPosts = posts => ({
   type: LOAD_POSTS,
   posts
@@ -18,3 +16,6 @@ export const sortPostsBy = field => ({
   type: SORT_POSTS_BY,
   field
 });
+
+
+
