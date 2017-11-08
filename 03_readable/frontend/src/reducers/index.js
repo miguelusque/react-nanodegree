6168dbfb3cde2472e421d8ee085d0c8d94d46de8
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-08 21:08:15 +0000

    Add "delete post" feature

--- 03_readable/frontend/src/reducers/index.js
+++ 03_readable/frontend/src/reducers/index.js
@@ -1,55 +1,80 @@
-import { LOAD_POSTS, SORT_POSTS_BY, FILTER_POSTS_BY, UPDATE_POST } from '../actions'
+import { LOAD_POSTS, SORT_POSTS_BY, FILTER_POSTS_BY, UPDATE_POST, DELETE_POST } from '../actions'
 import { SORTABLE_FIELDS } from '../components/SortPostsBy'
 
 const initialPostsState = {
   posts: [],
-  unfilteredPosts: [],
+  postsCache: [],
   sortedBy: SORTABLE_FIELDS.score,
   filteredBy: ''
 }
 
 const posts = (state = initialPostsState, action) => {
+  console.log('Action', action);
   switch (action.type) {
     case LOAD_POSTS:
       // When loading posts, sort then by the initial sorting field (state.sortedBy)
       return {
         ...state,
-        posts:action.posts.sort((a,b) => b[state.sortedBy] - a[state.sortedBy]),
-        unfilteredPosts: [...action.posts]
+        posts: dressPostsUp(action.posts, state.sortedBy),
+        postsCache: action.posts
       };
     case SORT_POSTS_BY:
       return {
         ...state,
-        posts: [...state.posts].sort((a,b) => b[action.field] - a[action.field]),
-        unfilteredPosts: [...state.unfilteredPosts].sort((a,b) => b[action.field] - a[action.field]),
+        posts: dressPostsUp(state.postsCache, action.field),
         sortedBy: action.field
       };
     case FILTER_POSTS_BY:
       return {
         ...state,
         posts: action.category === ''
-          ? [...state.unfilteredPosts]
-          : [...state.posts].filter(post => post.category === action.category),
+          ? dressPostsUp(state.postsCache, state.sortedBy)
+          : dressPostsUp(state.postsCache, state.sortedBy, action.category),
         filteredBy: action.category
       };
     case UPDATE_POST:
-      const updatedPost = {
-        ...state.posts.filter(post => post.id === action.updatedFields.id)[0],
-        ...action.updatedFields
-      };
+      const postsAfterUpdate = [
+        ...state.postsCache.filter(post => post.id !== action.updatedFields.id),
+        {
+          ...state.posts.filter(post => post.id === action.updatedFields.id)[0],
+          ...action.updatedFields
+        }];
 
       return {
         ...state,
-        posts: [
-          ...state.posts.filter(post => post.id !== action.updatedFields.id),
-          updatedPost].sort((a,b) => b[state.sortedBy] - a[state.sortedBy]),
-        unfilteredPosts: [
-          ...state.unfilteredPosts.filter(post => post.id !== action.updatedFields.id),
-          updatedPost].sort((a,b) => b[state.sortedBy] - a[state.sortedBy])
+        posts: dressPostsUp(postsAfterUpdate, state.sortedBy, state.filteredBy),
+        postsCache: postsAfterUpdate
       };
+    case DELETE_POST:
+      const postsAfterDelete = [...state.postsCache.filter(post => post.id !== action.postId) ];
+
+      return {
+        ...state,
+        posts: dressPostsUp(postsAfterDelete, state.sortedBy, state.filteredBy),
+        postsCache: postsAfterDelete
+      };
+
+
     default:
       return state;
   }
+}
+
+// This function dresses the posts up (sorts and filters them if requested).
+const dressPostsUp = (posts, sortedBy, filteredBy) => {
+  let result = [...posts];
+  if (sortedBy) {
+    console.log("sortedBy");
+    result.sort((a,b) => b[sortedBy] - a[sortedBy])
+  }
+
+  if (filteredBy) {
+    console.log("filteredBy");
+    result = result.filter(post => post.category === filteredBy)
+  }
+
+  console.log(result);
+  return result;
 }
 
 export default posts;
