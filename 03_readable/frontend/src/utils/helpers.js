dc96a785ccc9348f56f942c4e62619dbae90fb15
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-12 20:56:10 +0100

    Add uuidv4 function

--- 03_readable/frontend/src/utils/helpers.js
+++ 03_readable/frontend/src/utils/helpers.js
@@ -5,3 +5,12 @@ export function timestampToString (timestamp) {
   return a.getDate() + '-' + months[a.getMonth()] + '-' + a.getFullYear() + ' ' +
     a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
 }
+
+// This function calculates an uuid v4 complaint.
+// Credit: https://stackoverflow.com/a/2117523/3687310
+export function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : ((r & 0x3) | 0x8);
+    return v.toString(16);
+  });
+}
