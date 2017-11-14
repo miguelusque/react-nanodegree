4c7c38bf6cee7e8f75775ca692af8d004671743a
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-11-14 22:51:16 +0100

    Refactor to functional stateless component

--- 03_readable/frontend/src/components/ActionsToolBar.js
+++ 03_readable/frontend/src/components/ActionsToolBar.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import MdEdit from 'react-icons/lib/md/edit';
 import MdDelete from 'react-icons/lib/md/delete';
 import './css/ActionsToolBar.css';
 
-class ActionsToolBar extends Component {
-  static propTypes = {
-    onEdit: PropTypes.func,
-    onDelete: PropTypes.func
-  };
 
-  render() {
-    const {onEdit, onDelete} = this.props;
+const ActionsToolBar = ({onEdit, onDelete}) => (
+  <div className='actionsToolBar' unselectable="on">
+    <MdEdit className='actionsToolBarEditButton' onClick={onEdit}/>
+    <MdDelete className='actionsToolBarDeleteButton' onClick={onDelete}/>
+  </div>
+);
 
-    return (
-      <div className='actionsToolBar' unselectable="on">
-        <MdEdit className='actionsToolBarEditButton' onClick={onEdit}/>
-        <MdDelete className='actionsToolBarDeleteButton' onClick={onDelete}/>
-      </div>
-    )
-  }
-}
+ActionsToolBar.propTypes = {
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func
+};
 
 export default ActionsToolBar;
