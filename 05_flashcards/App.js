3fb24ceb40f8a3aa6fdd6feb144abf1894d59b76
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-12-06 21:43:14 +0100

    Add navigation

--- 05_flashcards/App.js
+++ 05_flashcards/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Text, View, StatusBar as NativeStatusBar } from 'react-native';
+import {View, Platform, StatusBar as NativeStatusBar} from 'react-native';
+import {TabNavigator, StackNavigator} from 'react-navigation';
 import PropTypes from 'prop-types';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import {Constants} from 'expo';
-import {purple} from './utils/colors';
-
+import {FontAwesome, MaterialIcons} from '@expo/vector-icons';
+import {purple, white} from './utils/colors';
+import AddDeck from './components/AddDeck';
+import DeckDetails from './components/DeckDetails';
+import Decks from './components/Decks';
+import reducer from './reducers';
 
 const StatusBar = ({backgroundColor, ... props}) => (
   <View style={{backgroundColor, height: Constants.statusBarHeight}}>
@@ -15,15 +22,64 @@ StatusBar.propTypes = {
   backgroundColor: PropTypes.string.isRequired
 };
 
+const Tabs = TabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      // eslint-disable-next-line react/display-name, react/prop-types
+      tabBarIcon: ({tintColor}) => <MaterialIcons name='library-books' size={30}
+        color={tintColor}/>
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      // eslint-disable-next-line react/display-name, react/prop-types
+      tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30}
+        color={tintColor}/>
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {width: 0, height: 3},
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+});
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  DeckDetails: {
+    screen: DeckDetails,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {backgroundColor: purple}
+    }
+  }
+});
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <StatusBar backgroundColor={purple} barStyle='light-content'/>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <StatusBar backgroundColor={purple} barStyle='light-content'/>
+          <MainNavigator/>
+        </View>
+      </Provider>
     );
   }
 }
