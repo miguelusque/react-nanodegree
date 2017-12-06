b2b9c7e2b1cc381aaf874e7b3e52f1e334fcacb0
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-12-06 15:42:05 +0100

    Add status bar

--- 05_flashcards/App.js
+++ 05_flashcards/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar as NativeStatusBar } from 'react-native';
+import PropTypes from 'prop-types';
+import {Constants} from 'expo';
+import {purple} from './utils/colors';
+
+
+const StatusBar = ({backgroundColor, ... props}) => (
+  <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+    <NativeStatusBar translucent backgroundColor={backgroundColor} {...props}/>
+  </View>
+);
+
+StatusBar.propTypes = {
+  backgroundColor: PropTypes.string.isRequired
+};
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={{flex: 1}}>
+        <StatusBar backgroundColor={purple} barStyle='light-content'/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -12,12 +27,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
