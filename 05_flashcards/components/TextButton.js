975cd729143a2f84b4eae75981b0e9f6c9bc364d
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-12-07 21:10:13 +0100

    Change interface to make it more reusable

--- 05_flashcards/components/TextButton.js
+++ 05_flashcards/components/TextButton.js
@@ -3,22 +3,21 @@ import {Text, TouchableOpacity, StyleSheet, Platform} from 'react-native';
 import PropTypes from 'prop-types';
 import {purple} from '../utils/colors';
 
-const TextButton = ({children, onPress, style={}}) => (
-  <TouchableOpacity onPress={onPress}>
-    <Text style={[styles.reset, style]}>{children}</Text>
+const TextButton = ({children, onPress, style={}, color={purple}}) => (
+  <TouchableOpacity style={[styles.reset, style]} onPress={onPress}>
+    <Text style={{color, textAlign: 'center'}}>{children}</Text>
   </TouchableOpacity>
 );
 
 TextButton.propTypes = {
   children: PropTypes.string.isRequired,
   onPress: PropTypes.func.isRequired,
-  style: PropTypes.object
+  style: PropTypes.object,
+  color: PropTypes.string
 };
 
 const styles = StyleSheet.create({
   reset: {
-    textAlign: 'center',
-    color: purple,
     borderRadius: Platform.OS === 'ios' ? 16 : 6,
     borderColor: purple,
     borderWidth: 1,
